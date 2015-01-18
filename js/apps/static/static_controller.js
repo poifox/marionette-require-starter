a0505d0d13e0bc89fa9e37aeb82090dfaf84ba29
Jonathan Acosta <jota.aco@gmail.com>
2015-01-17 18:51:31 -0600

    Modified the StaticApp.Controller module so it require()s the view files from the action handler and not load from the very beginning.

--- js/apps/static/static_controller.js
+++ js/apps/static/static_controller.js
@@ -1,22 +1,29 @@
-define(["app", "apps/static/views/all"], function(App) {
+define(["app"], function(App) {
 
 	// The StaticApp just takes care of rendering static pages,
 	// so there's no entities for them, or any complicated setup;
 	// all these handlers do is loading the views and their templates
 	// and immediately render them to the screen as soon as loaded.
+	//
+	// You can just copy and paste and rename variable in order to
+	// get a new static page for your site, and add the route to `pages_app.js`
 
 	App.module("StaticApp", function(StaticApp, App, Backbone, Marionette, $, _) {
 
 		StaticApp.Controller = {
 
 			home: function() {
-				var homePage = new StaticApp.Views.HomeView();
-				App.contentRegion.show(homePage);
+				require(["apps/static/views/home"], function(HomeView) {
+					var homePage = new HomeView();
+					App.contentRegion.show(homePage);
+				});
 			},
 
 			about: function() {
-				var homePage = new StaticApp.Views.AboutView();
-				App.contentRegion.show(homePage);
+				require(["apps/static/views/about"], function(AboutView) {
+					var homePage = new AboutView();
+					App.contentRegion.show(homePage);
+				});
 			}
 
 		};
